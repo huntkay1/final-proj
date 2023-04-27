be82446a6e1bd9c8add6bbaf3c6514bb54e3dedd
huntkay1 <huntkay1@msu.edu>
2023-04-27 00:02:33 -0400

    started layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import logo from './blab.png';
+import banner from './banner.jpg';
+import menu from './menu.jpeg';
+import mario from './mario.jpg';
+import dnd from './dnd.jpg';
+import Carousel from 'react-bootstrap/Carousel';
 import './App.css';
+
+ 
+
+function Navbar() {
+  return(
+    <nav className = "Navbar">
+    <img src={logo} className="App-logo"></img>
+
+    <form className="search-bar">
+    <input className="search-area" type="search" placeholder="what are we watching?" />
+      <button></button>
+    </form>
+
+    <ul className="navbar-nav">
+      <li className="nav-item">Log In</li>
+      <li className="nav-item">Sign Up</li>
+    </ul>
+      
+  </nav>
+  )
+}
+
+function Banner() {
+  return(
+  <img src={banner} className="banner" alt="Everything,Everywhere,All at Once Scene"></img>
+  )
+}
+
+function ImageCarousel() {
+  return(
+    <Carousel>
+      <Carousel.Item>
+        <img className="carousel-img" src={menu}></img>
+        <img className="carousel-img" src={mario}></img>
+        <img className="carousel-img" src={dnd}></img>
+        <Carousel.Caption>
+          <p>rating here</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Navbar />
+      <Banner />
+      <ImageCarousel />
     </div>
   );
 }
